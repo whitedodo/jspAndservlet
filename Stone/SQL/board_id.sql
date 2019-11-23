d88fe18db05101e2a472c0321bbe6d29037edbaf
Rabbitwhite <34395602+whitedodo@users.noreply.github.com>
2019-11-23 13:00:26 +0900

    Update board_id.sql

--- Stone/SQL/board_id.sql
+++ Stone/SQL/board_id.sql
@@ -11,6 +11,21 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- 테이블 rabbit2me.board_list 구조 내보내기
+CREATE TABLE IF NOT EXISTS `board_list` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `postType` varchar(50) DEFAULT NULL,
+  `subject` varchar(50) DEFAULT NULL,
+  `memo` text DEFAULT NULL,
+  `author` varchar(50) DEFAULT NULL,
+  `passwd` varchar(50) DEFAULT NULL,
+  `commentCnt` bigint(20) DEFAULT NULL,
+  `ip` varchar(50) DEFAULT NULL,
+  `regidate` datetime DEFAULT NULL,
+  `count` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
